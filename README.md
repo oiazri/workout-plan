8196241aafcb05c065688dde18900c6058d20829
oiazri <boysas09@gmail.com>
2025-06-26 00:06:54 +0700

    app workout plan built with react

---
+++ README.md
@@ -0,0 +1,3 @@
+# Workout Plan
+
+**WORKOUT PLAN** is a simple fitness planning web app built with **React**
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <div>
+      <h1>Workout Plan</h1>
+    </div>
+  );
+}
