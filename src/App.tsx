926864af26af33d78c2f979d06d827ec8315f998
oiazri <boysas09@gmail.com>
2025-06-30 13:41:16 +0700

    create congratulations componen

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ export function App() {
   return (
     <div>
       <h1>Workout Plan</h1>
+      <Congratulations />
     </div>
   );
+}
+
+export function Congratulations() {
+  return <h1>Good job!</h1>;
 }
