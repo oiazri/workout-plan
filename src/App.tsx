4f61b9bbc18e142afbc67ba6756f3121dca18035
oiazri <boysas09@gmail.com>
2025-07-06 19:48:17 +0700

    adding component fucntion profile gallery and section

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,63 @@ export function App() {
   return (
     <div>
       <h1>Workout Plan</h1>
-      <Congratulations />
+      <Gallery />
     </div>
   );
 }
 
-export function Congratulations() {
-  return <h1>Good job!</h1>;
+export function Gallery() {
+  return (
+    <section>
+      <h1>Anime Workout Motivation</h1>
+      <Profile />
+      <ProfileOne />
+      <ProfileTwo />
+    </section>
+  );
+}
+
+export function Profile() {
+  const fullname = "Son Goku";
+  return (
+    <div>
+      <h3>{fullname}</h3>
+      <img
+        src="https://i.pinimg.com/736x/9c/c5/b9/9cc5b9440b46dbda663a2487c84d0f0d.jpg"
+        alt={fullname}
+        height={150}
+        width={100}
+      />
+    </div>
+  );
+}
+
+export function ProfileOne() {
+  const fullname = "Saitama";
+  return (
+    <div>
+      <h3>{fullname}</h3>
+      <img
+        src="https://i.pinimg.com/736x/33/aa/d8/33aad8ac8fc010cd94b0253963505af5.jpg"
+        alt={fullname}
+        height={150}
+        width={100}
+      />
+    </div>
+  );
+}
+
+export function ProfileTwo() {
+  const fullname = "Baki Hanma";
+  return (
+    <div>
+      <h3>{fullname}</h3>
+      <img
+        src="https://i.pinimg.com/736x/1e/df/fd/1edffd083a98fb70bbde6a2109d539b0.jpg"
+        alt={fullname}
+        height={150}
+        width={100}
+      />
+    </div>
+  );
 }
