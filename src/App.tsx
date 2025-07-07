a991d3bf843efed59ce5b434c439bdd1ed12aac6
M Haidar Hanif <git@mhaidarhanif.com>
2025-07-07 16:32:05 +0700

    refactor profile component to use props

--- src/App.tsx
+++ src/App.tsx
@@ -11,54 +11,39 @@ export function Gallery() {
   return (
     <section>
       <h1>Anime Workout Motivation</h1>
-      <Profile />
-      <ProfileOne />
-      <ProfileTwo />
+      <Profile
+        fullname="Son Goku"
+        imageUrl="https://i.pinimg.com/736x/9c/c5/b9/9cc5b9440b46dbda663a2487c84d0f0d.jpg"
+        description="Saiyan who saves earth"
+      />
+      <Profile
+        fullname="Saitama"
+        imageUrl="https://i.pinimg.com/736x/33/aa/d8/33aad8ac8fc010cd94b0253963505af5.jpg"
+        description="Break his limiter"
+      />
+      <Profile
+        fullname="Baki"
+        imageUrl="https://i.pinimg.com/736x/1e/df/fd/1edffd083a98fb70bbde6a2109d539b0.jpg"
+        description="Want to surpass his father"
+      />
     </section>
   );
 }
 
-export function Profile() {
-  const fullname = "Son Goku";
+export function Profile({
+  fullname,
+  imageUrl,
+  description,
+}: {
+  fullname: string;
+  imageUrl: string;
+  description: string;
+}) {
   return (
     <div>
       <h3>{fullname}</h3>
-      <img
-        src="https://i.pinimg.com/736x/9c/c5/b9/9cc5b9440b46dbda663a2487c84d0f0d.jpg"
-        alt={fullname}
-        height={150}
-        width={100}
-      />
-    </div>
-  );
-}
-
-export function ProfileOne() {
-  const fullname = "Saitama";
-  return (
-    <div>
-      <h3>{fullname}</h3>
-      <img
-        src="https://i.pinimg.com/736x/33/aa/d8/33aad8ac8fc010cd94b0253963505af5.jpg"
-        alt={fullname}
-        height={150}
-        width={100}
-      />
-    </div>
-  );
-}
-
-export function ProfileTwo() {
-  const fullname = "Baki Hanma";
-  return (
-    <div>
-      <h3>{fullname}</h3>
-      <img
-        src="https://i.pinimg.com/736x/1e/df/fd/1edffd083a98fb70bbde6a2109d539b0.jpg"
-        alt={fullname}
-        height={150}
-        width={100}
-      />
+      <img src={imageUrl} alt={fullname} height={150} width={100} />
+      <p>{description}</p>
     </div>
   );
 }
