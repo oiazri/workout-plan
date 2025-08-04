4b7f507f91a0f27d60121c4873ba6843f3b5f9f9
M Haidar Hanif <git@mhaidarhanif.com>
2025-08-04 14:54:38 +0700

    profile props isHuman for conditional

--- src/App.tsx
+++ src/App.tsx
@@ -14,29 +14,40 @@ export function Gallery() {
       <Profile
         fullname="Son Goku"
         image="https://i.pinimg.com/736x/9c/c5/b9/9cc5b9440b46dbda663a2487c84d0f0d.jpg"
+        isHuman={false}
       />
       <Profile
         fullname="Saitama"
         image="https://i.pinimg.com/736x/33/aa/d8/33aad8ac8fc010cd94b0253963505af5.jpg"
+        isHuman={true}
       />
       <Profile
         fullname="Baki Hanma"
         image="https://i.pinimg.com/736x/1e/df/fd/1edffd083a98fb70bbde6a2109d539b0.jpg"
+        isHuman={true}
+      />
+      <Profile
+        fullname="Superman"
+        image="https://i.pinimg.com/236x/78/a3/5e/78a35eae6a4be585e0e8766618a037a3.jpg"
+        isHuman={false}
       />
     </section>
   );
 }
 
-export function Profile({
-  fullname,
-  image,
-}: {
-  fullname: string;
-  image: string;
-}) {
+export function Profile({ fullname, image, isHuman }: { fullname: string; image: string; isHuman: boolean }) {
+  if (!isHuman) {
+    return (
+      <div>
+        <h3>{fullname} is NOT HUMAN</h3>
+        <img src={image} alt={fullname} height={150} width={100} />
+      </div>
+    );
+  }
+
   return (
     <div>
-      <h3>{fullname}</h3>
+      <h3>{fullname} is HUMAN</h3>
       <img src={image} alt={fullname} height={150} width={100} />
     </div>
   );
