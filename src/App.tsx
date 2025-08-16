0d59fdb29e573d1b7c1ca7a8fe6da25f1608b056
M Haidar Hanif <git@mhaidarhanif.com>
2025-08-16 15:00:37 +0700

    prepare workout description, remove isHuman

--- src/App.tsx
+++ src/App.tsx
@@ -14,22 +14,18 @@ export function Gallery() {
       <Profile
         fullname="Son Goku"
         image="https://i.pinimg.com/736x/9c/c5/b9/9cc5b9440b46dbda663a2487c84d0f0d.jpg"
-        isHuman={false}
       />
       <Profile
         fullname="Saitama"
         image="https://i.pinimg.com/736x/33/aa/d8/33aad8ac8fc010cd94b0253963505af5.jpg"
-        isHuman={true}
       />
       <Profile
         fullname="Baki Hanma"
         image="https://i.pinimg.com/736x/1e/df/fd/1edffd083a98fb70bbde6a2109d539b0.jpg"
-        isHuman={true}
       />
       <Profile
         fullname="kamado tanjiro"
         image="https://i.pinimg.com/736x/04/07/ec/0407ec63d788a16c2f6357138a893021.jpg"
-        isHuman={true}
       />
     </section>
   );
@@ -38,17 +34,13 @@ export function Gallery() {
 export function Profile({
   fullname,
   image,
-  isHuman,
 }: {
   fullname: string;
   image: string;
-  isHuman: boolean;
 }) {
   return (
-    <div className="bg-orange-100">
-      <h3>
-        {fullname} is {isHuman ? "HUMAN" : "NOT HUMAN"}
-      </h3>
+    <div className="bg-green-200 text-green-900">
+      <h3 className="text-lg font-bold">{fullname}</h3>
       <img src={image} alt={fullname} height={150} width={100} />
     </div>
   );
