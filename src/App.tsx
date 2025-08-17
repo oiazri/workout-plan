ed0e175c7b88c0afedc3be23f6aa62616617b107
oiazri <boysas09@gmail.com>
2025-08-17 18:12:43 +0700

    Rendering list with array of characters

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,113 @@
 export function App() {
   return (
-    <div>
+    <div className="m-10">
       <h1>Workout Plan</h1>
       <Gallery />
     </div>
   );
 }
 
+type Character = {
+  id: number;
+  fullname: string;
+  imageUrl: string;
+  description: string;
+  exercises: string[];
+};
+
+const characters: Character[] = [
+  {
+    id: 1,
+    fullname: "Son Goku",
+    imageUrl:
+      "https://i.pinimg.com/736x/9c/c5/b9/9cc5b9440b46dbda663a2487c84d0f0d.jpg",
+    description:
+      "rain beyond limits, but always with purpose. Each battle makes you stronger.",
+    exercises: ["Deadlift", "Squats", "Bench press", "Barbell row"],
+  },
+  {
+    id: 2,
+    fullname: "Vegeta",
+    imageUrl:
+      "https://i.pinimg.com/736x/0f/15/49/0f15491beeb061f90c67bab4ea6b3955.jpg",
+    description: "Every day, you must surpass your past self.",
+    exercises: [
+      "Upper body",
+      "Lower body",
+      "HIIT",
+      "Strength & Combat Conditioning",
+    ],
+  },
+  {
+    id: 3,
+    fullname: "Saitama",
+    imageUrl:
+      "https://i.pinimg.com/736x/33/aa/d8/33aad8ac8fc010cd94b0253963505af5.jpg",
+    description: "No shortcuts. Just do it every day, rain or shine.",
+    exercises: ["100 push-ups", "100 sit-ups", "100 squats", "10km run"],
+  },
+  {
+    id: 4,
+    fullname: "Baki Hanma",
+    imageUrl:
+      "https://i.pinimg.com/736x/1e/df/fd/1edffd083a98fb70bbde6a2109d539b0.jpg",
+    description: "A strong body is nothing without willpower and adaptability.",
+    exercises: [
+      "One-arm-push-up",
+      "Pistol squats",
+      "Hanging leg raises",
+      "Shadow punch",
+    ],
+  },
+  {
+    id: 5,
+    fullname: "Kamado Tanjiro",
+    imageUrl:
+      "https://i.pinimg.com/736x/04/07/ec/0407ec63d788a16c2f6357138a893021.jpg",
+    description:
+      "Control your breath, control your spirit. Strength flows from calm focus.",
+    exercises: ["Pulls-up", "Push-up", "Weighted Lunges", " Box Breathing"],
+  },
+];
+
 export function Gallery() {
   return (
     <section>
       <h1>Anime Workout Motivation</h1>
-      <Profile
-        fullname="Son Goku"
-        image="https://i.pinimg.com/736x/9c/c5/b9/9cc5b9440b46dbda663a2487c84d0f0d.jpg"
-        isHuman={false}
-      />
-      <Profile
-        fullname="Saitama"
-        image="https://i.pinimg.com/736x/33/aa/d8/33aad8ac8fc010cd94b0253963505af5.jpg"
-        isHuman={true}
-      />
-      <Profile
-        fullname="Baki Hanma"
-        image="https://i.pinimg.com/736x/1e/df/fd/1edffd083a98fb70bbde6a2109d539b0.jpg"
-        isHuman={true}
-      />
-      <Profile
-        fullname="kamado tanjiro"
-        image="https://i.pinimg.com/736x/04/07/ec/0407ec63d788a16c2f6357138a893021.jpg"
-        isHuman={true}
-      />
+
+      <ul className="grid grid-cols-3 gap-4">
+        {characters.map((character) => (
+          <li key={character.id}>
+            <Profile character={character} />
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
 
-export function Profile({
-  fullname,
-  image,
-  isHuman,
-}: {
-  fullname: string;
-  image: string;
-  isHuman: boolean;
-}) {
-  if (!isHuman) {
-    return (
-      <div className="bg-orange-200">
-        <h3>{fullname} is NOT HUMAN</h3>
-        <img src={image} alt={fullname} height={150} width={100} />
-      </div>
-    );
-  }
-
+export function Profile({ character }: { character: Character }) {
   return (
-    <div>
-      <h3>{fullname} is HUMAN</h3>
-      <img src={image} alt={fullname} height={150} width={100} />
+    <div className="min-h-56 rounded flex gap-4 p-4 bg-yellow-300 text-black">
+      <div>
+        <img
+          className="rounded"
+          src={character.imageUrl}
+          alt={character.fullname}
+          height={150}
+          width={100}
+        />
+      </div>
+
+      <div>
+        <h3 className="text-lg font-bold">{character.fullname}</h3>
+        <p>{character.description}</p>
+        <ul className="list-disc list-inside">
+          {character.exercises.map((exercise) => (
+            <li key={exercise}>{exercise}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
