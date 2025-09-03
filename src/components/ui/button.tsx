102ba3cea7a02cca70ccdfa7ca0657357b7df894
M Haidar Hanif <git@mhaidarhanif.com>
2025-09-03 16:41:22 +0700

    simplify button color condition

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,16 +5,14 @@ export function Button({
   color: string;
   children: React.ReactNode;
 }) {
-  if (color === "green") {
-    return (
-      <button className="bg-green-600 py-1 px-2 rounded text-green-100 hover:bg-green-700">
-        {children}
-      </button>
-    );
-  }
-
   return (
-    <button className="bg-red-600 py-1 px-2 rounded text-red-100 hover:bg-red-700">
+    <button
+      className={
+        "py-1 px-2 rounded " + color === "green"
+          ? "bg-green-600 text-green-100 hover:bg-green-700"
+          : "bg-red-600 text-red-100 hover:bg-red-700"
+      }
+    >
       {children}
     </button>
   );
